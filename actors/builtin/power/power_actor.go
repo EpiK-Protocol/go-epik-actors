de9fc7122817362eaf2051ebaa1056dc61e7f967
Larry <larry@epik-protocol.io>
2020-09-18 16:04:39 +0800

    tmp fix chain sync

--- actors/builtin/power/power_actor.go
+++ actors/builtin/power/power_actor.go
@@ -246,15 +246,17 @@ type OnFaultBeginParams struct {
 func (a Actor) OnFaultBegin(rt Runtime, params *OnFaultBeginParams) *adt.EmptyValue {
 	rt.ValidateImmediateCallerType(builtin.StorageMinerActorCodeID)
 	// TODO: larry need not update power for fault
-	// var st State
-	// rt.State().Transaction(&st, func() interface{} {
-	// 	rbpower, qapower := powersForWeights(params.Weights)
-	// 	err := st.AddToClaim(adt.AsStore(rt), rt.Message().Caller(), rbpower.Neg(), qapower.Neg())
-	// 	if err != nil {
-	// 		rt.Abortf(exitcode.ErrIllegalState, "failed to deduct claimed power for sector: %v", err)
-	// 	}
-	// 	return nil
-	// })
+	if rt.CurrEpoch() <= 13157 {
+		var st State
+		rt.State().Transaction(&st, func() interface{} {
+			rbpower, qapower := powersForWeights(params.Weights)
+			err := st.AddToClaim(adt.AsStore(rt), rt.Message().Caller(), rbpower.Neg(), qapower.Neg())
+			if err != nil {
+				rt.Abortf(exitcode.ErrIllegalState, "failed to deduct claimed power for sector: %v", err)
+			}
+			return nil
+		})
+	}
 	return nil
 }
 
@@ -266,16 +268,17 @@ func (a Actor) OnFaultEnd(rt Runtime, params *OnFaultEndParams) *adt.EmptyValue 
 	rt.ValidateImmediateCallerType(builtin.StorageMinerActorCodeID)
 
 	// TODO: larry need not update power for fault
-	// var st State
-	// rt.State().Transaction(&st, func() interface{} {
-	// 	rbpower, qapower := powersForWeights(params.Weights)
-	// 	err := st.AddToClaim(adt.AsStore(rt), rt.Message().Caller(), rbpower, qapower)
-	// 	if err != nil {
-	// 		rt.Abortf(exitcode.ErrIllegalState, "failed to add claimed power for sector: %v", err)
-	// 	}
-	// 	return nil
-	// })
-
+	if rt.CurrEpoch() <= 13157 {
+		var st State
+		rt.State().Transaction(&st, func() interface{} {
+			rbpower, qapower := powersForWeights(params.Weights)
+			err := st.AddToClaim(adt.AsStore(rt), rt.Message().Caller(), rbpower, qapower)
+			if err != nil {
+				rt.Abortf(exitcode.ErrIllegalState, "failed to add claimed power for sector: %v", err)
+			}
+			return nil
+		})
+	}
 	return nil
 }
 
