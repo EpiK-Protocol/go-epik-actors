39e9cb90f9b7a9b1b12b37323eb3f384cb653f6e
Alex <445306+anorth@users.noreply.github.com>
2020-12-03 13:48:07 +1100

    Check consensus fault target is receiving actor (#1314)
    Co-authored-by: zgfzgf <48779939+zgfzgf@users.noreply.github.com>

--- actors/builtin/miner/miner_actor.go
+++ actors/builtin/miner/miner_actor.go
@@ -1606,8 +1606,9 @@ type ReportConsensusFaultParams struct {
 }
 
 func (a Actor) ReportConsensusFault(rt Runtime, params *ReportConsensusFaultParams) *abi.EmptyValue {
-	// Note: only the first reporter of any fault is rewarded.
-	// Subsequent invocations fail because the target miner has been removed.
+	// Note: only the first report of any fault is processed because it sets the
+	// ConsensusFaultElapsed state variable to an epoch after the fault, and reports prior to
+	// that epoch are no longer valid.
 	rt.ValidateImmediateCallerType(builtin.CallerTypesSignable...)
 	reporter := rt.Caller()
 
@@ -1615,12 +1616,15 @@ func (a Actor) ReportConsensusFault(rt Runtime, params *ReportConsensusFaultPara
 	if err != nil {
 		rt.Abortf(exitcode.ErrIllegalArgument, "fault not verified: %s", err)
 	}
+	if fault.Target != rt.Receiver() {
+		rt.Abortf(exitcode.ErrIllegalArgument, "fault by %v reported to miner %v", fault.Target, rt.Receiver())
+	}
 
 	// Elapsed since the fault (i.e. since the higher of the two blocks)
 	currEpoch := rt.CurrEpoch()
 	faultAge := currEpoch - fault.Epoch
 	if faultAge <= 0 {
-		rt.Abortf(exitcode.ErrIllegalArgument, "invalid fault epoch %v ahead of current %v", fault.Epoch, rt.CurrEpoch())
+		rt.Abortf(exitcode.ErrIllegalArgument, "invalid fault epoch %v ahead of current %v", fault.Epoch, currEpoch)
 	}
 
 	// Penalize miner consensus fault fee
@@ -1658,7 +1662,7 @@ func (a Actor) ReportConsensusFault(rt Runtime, params *ReportConsensusFaultPara
 		rewardAmount = big.Min(burnAmount, slasherReward)
 		// reduce burnAmount by rewardAmount
 		burnAmount = big.Sub(burnAmount, rewardAmount)
-		info.ConsensusFaultElapsed = rt.CurrEpoch() + ConsensusFaultIneligibilityDuration
+		info.ConsensusFaultElapsed = currEpoch + ConsensusFaultIneligibilityDuration
 		err = st.SaveInfo(adt.AsStore(rt), info)
 		builtin.RequireNoErr(rt, err, exitcode.ErrSerialization, "failed to save miner info")
 	})
