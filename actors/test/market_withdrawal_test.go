filecoin-project/specs-actors

Inject block store into VM and sim (#1342)

actors/test/market_withdrawal_test.go
@@ -10,12 +10,13 @@ import (
 
 	"github.com/filecoin-project/specs-actors/v3/actors/builtin"
 	"github.com/filecoin-project/specs-actors/v3/actors/builtin/market"
+	"github.com/filecoin-project/specs-actors/v3/support/ipld"
 	vm "github.com/filecoin-project/specs-actors/v3/support/vm"
 )
 
 func TestMarketWithdraw(t *testing.T) {
 	ctx := context.Background()
-	v := vm.NewVMWithSingletons(ctx, t)
+	v := vm.NewVMWithSingletons(ctx, t, ipld.NewBlockStoreInMemory())
 	initialBalance := big.Mul(big.NewInt(6), big.NewInt(1e18))
 	addrs := vm.CreateAccounts(ctx, t, v, 1, initialBalance, 93837778)
 	caller := addrs[0]

actors/test/power_scenario_test.go
@@ -11,12 +11,13 @@ import (
 	"github.com/filecoin-project/specs-actors/v3/actors/builtin"
 	"github.com/filecoin-project/specs-actors/v3/actors/builtin/miner"
 	"github.com/filecoin-project/specs-actors/v3/actors/builtin/power"
+	"github.com/filecoin-project/specs-actors/v3/support/ipld"
 	vm "github.com/filecoin-project/specs-actors/v3/support/vm"
 )
 
 func TestCreateMiner(t *testing.T) {
 	ctx := context.Background()
-	v := vm.NewVMWithSingletons(ctx, t)
+	v := vm.NewVMWithSingletons(ctx, t, ipld.NewBlockStoreInMemory())
 	addrs := vm.CreateAccounts(ctx, t, v, 1, big.Mul(big.NewInt(10_000), big.NewInt(1e18)), 93837778)
 
 	params := power.CreateMinerParams{
@@ -67,7 +68,7 @@ func TestCreateMiner(t *testing.T) {
 
 func TestOnEpochTickEnd(t *testing.T) {
 	ctx := context.Background()
-	v := vm.NewVMWithSingletons(ctx, t)
+	v := vm.NewVMWithSingletons(ctx, t, ipld.NewBlockStoreInMemory())
 	addrs := vm.CreateAccounts(ctx, t, v, 1, big.Mul(big.NewInt(10_000), big.NewInt(1e18)), 93837778)
 
 	// create a miner

actors/test/terminate_sectors_scenario_test.go
@@ -16,6 +16,7 @@ import (
 	"github.com/filecoin-project/specs-actors/v3/actors/builtin/power"
 	"github.com/filecoin-project/specs-actors/v3/actors/builtin/verifreg"
 	"github.com/filecoin-project/specs-actors/v3/actors/runtime/proof"
+	"github.com/filecoin-project/specs-actors/v3/support/ipld"
 	tutil "github.com/filecoin-project/specs-actors/v3/support/testing"
 	vm "github.com/filecoin-project/specs-actors/v3/support/vm"
 )
@@ -23,7 +24,7 @@ import (
 // This scenario hits all Market Actor methods.
 func TestTerminateSectors(t *testing.T) {
 	ctx := context.Background()
-	v := vm.NewVMWithSingletons(ctx, t)
+	v := vm.NewVMWithSingletons(ctx, t, ipld.NewBlockStoreInMemory())
 	addrs := vm.CreateAccounts(ctx, t, v, 4, big.Mul(big.NewInt(10_000), vm.FIL), 93837778)
 	owner, verifier, unverifiedClient, verifiedClient := addrs[0], addrs[1], addrs[2], addrs[3]
 	worker := owner

support/agent/cases_test.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
+	cbor "github.com/ipfs/go-ipld-cbor"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
@@ -16,6 +17,7 @@ import (
 	"github.com/filecoin-project/specs-actors/v3/actors/builtin/power"
 	"github.com/filecoin-project/specs-actors/v3/actors/states"
 	"github.com/filecoin-project/specs-actors/v3/support/agent"
+	"github.com/filecoin-project/specs-actors/v3/support/ipld"
 	vm_test "github.com/filecoin-project/specs-actors/v3/support/vm"
 )
 
@@ -26,7 +28,7 @@ func TestCreate20Miners(t *testing.T) {
 
 	rnd := rand.New(rand.NewSource(42))
 
-	sim := agent.NewSim(ctx, t, agent.SimConfig{Seed: rnd.Int63()})
+	sim := agent.NewSim(ctx, t, newBlockStore, agent.SimConfig{Seed: rnd.Int63()})
 	accounts := vm_test.CreateAccounts(ctx, t, sim.GetVM(), minerCount, initialBalance, rnd.Int63())
 	sim.AddAgent(agent.NewMinerGenerator(
 		accounts,
@@ -74,7 +76,7 @@ func Test500Epochs(t *testing.T) {
 
 	// set up sim
 	rnd := rand.New(rand.NewSource(42))
-	sim := agent.NewSim(ctx, t, agent.SimConfig{
+	sim := agent.NewSim(ctx, t, newBlockStore, agent.SimConfig{
 		Seed:             rnd.Int63(),
 		CheckpointEpochs: 1000,
 	})
@@ -152,7 +154,7 @@ func TestCommitPowerAndCheckInvariants(t *testing.T) {
 	minerCount := 1
 
 	rnd := rand.New(rand.NewSource(42))
-	sim := agent.NewSim(ctx, t, agent.SimConfig{Seed: rnd.Int63()})
+	sim := agent.NewSim(ctx, t, newBlockStore, agent.SimConfig{Seed: rnd.Int63()})
 	accounts := vm_test.CreateAccounts(ctx, t, sim.GetVM(), minerCount, initialBalance, rnd.Int63())
 	sim.AddAgent(agent.NewMinerGenerator(
 		accounts,
@@ -207,7 +209,7 @@ func TestCommitAndCheckReadWriteStats(t *testing.T) {
 
 	// set up sim
 	rnd := rand.New(rand.NewSource(42))
-	sim := agent.NewSim(ctx, t, agent.SimConfig{
+	sim := agent.NewSim(ctx, t, newBlockStore, agent.SimConfig{
 		Seed:             rnd.Int63(),
 		CheckpointEpochs: 1000,
 	})
@@ -284,7 +286,7 @@ func TestCreateDeals(t *testing.T) {
 
 	// set up sim
 	rnd := rand.New(rand.NewSource(42))
-	sim := agent.NewSim(ctx, t, agent.SimConfig{Seed: rnd.Int63()})
+	sim := agent.NewSim(ctx, t, newBlockStore, agent.SimConfig{Seed: rnd.Int63()})
 
 	// create miners
 	workerAccounts := vm_test.CreateAccounts(ctx, t, sim.GetVM(), minerCount, initialBalance, rnd.Int63())
@@ -357,7 +359,7 @@ func TestCCUpgrades(t *testing.T) {
 
 	// set up sim
 	rnd := rand.New(rand.NewSource(42))
-	sim := agent.NewSim(ctx, t, agent.SimConfig{Seed: rnd.Int63()})
+	sim := agent.NewSim(ctx, t, newBlockStore, agent.SimConfig{Seed: rnd.Int63()})
 
 	// create miners
 	workerAccounts := vm_test.CreateAccounts(ctx, t, sim.GetVM(), minerCount, initialBalance, rnd.Int63())
@@ -428,6 +430,10 @@ func TestCCUpgrades(t *testing.T) {
 				sim.MessageCount, deals, upgrades)
 		}
 	}
+}
+
+func newBlockStore() cbor.IpldBlockstore {
+	return ipld.NewBlockStoreInMemory()
 }
 
 func printCallStats(method vm_test.MethodKey, stats *vm_test.CallStats, indent string) { // nolint:unused

EpiK-Protocol/go-epik-actors

actors/test/market_withdrawal_test.go
@@ -10,12 +10,13 @@ import (
 
 	"github.com/filecoin-project/specs-actors/v2/actors/builtin"
 	"github.com/filecoin-project/specs-actors/v2/actors/builtin/market"
+	"github.com/filecoin-project/specs-actors/v2/support/ipld"
 	vm "github.com/filecoin-project/specs-actors/v2/support/vm"
 )
 
 func TestMarketWithdraw(t *testing.T) {
 	ctx := context.Background()
-	v := vm.NewVMWithSingletons(ctx, t)
+	v := vm.NewVMWithSingletons(ctx, t, ipld.NewBlockStoreInMemory())
 	initialBalance := big.Mul(big.NewInt(6), big.NewInt(1e18))
 	addrs := vm.CreateAccounts(ctx, t, v, 1, initialBalance, 93837778)
 	caller := addrs[0]

actors/test/power_scenario_test.go
@@ -11,12 +11,13 @@ import (
 	"github.com/filecoin-project/specs-actors/v2/actors/builtin"
 	"github.com/filecoin-project/specs-actors/v2/actors/builtin/miner"
 	"github.com/filecoin-project/specs-actors/v2/actors/builtin/power"
+	"github.com/filecoin-project/specs-actors/v2/support/ipld"
 	vm "github.com/filecoin-project/specs-actors/v2/support/vm"
 )
 
 func TestCreateMiner(t *testing.T) {
 	ctx := context.Background()
-	v := vm.NewVMWithSingletons(ctx, t)
+	v := vm.NewVMWithSingletons(ctx, t, ipld.NewBlockStoreInMemory())
 	addrs := vm.CreateAccounts(ctx, t, v, 1, big.Mul(big.NewInt(10_000), big.NewInt(1e18)), 93837778)
 
 	params := power.CreateMinerParams{
@@ -67,7 +68,7 @@ func TestCreateMiner(t *testing.T) {
 
 func TestOnEpochTickEnd(t *testing.T) {
 	ctx := context.Background()
-	v := vm.NewVMWithSingletons(ctx, t)
+	v := vm.NewVMWithSingletons(ctx, t, ipld.NewBlockStoreInMemory())
 	addrs := vm.CreateAccounts(ctx, t, v, 1, big.Mul(big.NewInt(10_000), big.NewInt(1e18)), 93837778)
 
 	// create a miner

actors/test/terminate_sectors_scenario_test.go
@@ -15,6 +15,7 @@ import (
 	"github.com/filecoin-project/specs-actors/v2/actors/builtin/miner"
 	"github.com/filecoin-project/specs-actors/v2/actors/builtin/power"
 	"github.com/filecoin-project/specs-actors/v2/actors/runtime/proof"
+	"github.com/filecoin-project/specs-actors/v2/support/ipld"
 	tutil "github.com/filecoin-project/specs-actors/v2/support/testing"
 	vm "github.com/filecoin-project/specs-actors/v2/support/vm"
 )
@@ -22,7 +23,7 @@ import (
 // This scenario hits all Market Actor methods.
 func TestTerminateSectors(t *testing.T) {
 	ctx := context.Background()
-	v := vm.NewVMWithSingletons(ctx, t)
+	v := vm.NewVMWithSingletons(ctx, t, ipld.NewBlockStoreInMemory())
 	addrs := vm.CreateAccounts(ctx, t, v, 4, big.Mul(big.NewInt(10_000), vm.FIL), 93837778)
 	owner /* , verifier */, unverifiedClient, verifiedClient := addrs[0] /* , addrs[1] */, addrs[2], addrs[3]
 	worker := owner

support/agent/cases_test.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
+	cbor "github.com/ipfs/go-ipld-cbor"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
@@ -16,6 +17,7 @@ import (
 	"github.com/filecoin-project/specs-actors/v2/actors/builtin/power"
 	"github.com/filecoin-project/specs-actors/v2/actors/states"
 	"github.com/filecoin-project/specs-actors/v2/support/agent"
+	"github.com/filecoin-project/specs-actors/v2/support/ipld"
 	vm_test "github.com/filecoin-project/specs-actors/v2/support/vm"
 )
 
@@ -26,7 +28,7 @@ func TestCreate20Miners(t *testing.T) {
 
 	rnd := rand.New(rand.NewSource(42))
 
-	sim := agent.NewSim(ctx, t, agent.SimConfig{Seed: rnd.Int63()})
+	sim := agent.NewSim(ctx, t, newBlockStore, agent.SimConfig{Seed: rnd.Int63()})
 	accounts := vm_test.CreateAccounts(ctx, t, sim.GetVM(), minerCount, initialBalance, rnd.Int63())
 	sim.AddAgent(agent.NewMinerGenerator(
 		accounts,
@@ -74,7 +76,7 @@ func Test500Epochs(t *testing.T) {
 
 	// set up sim
 	rnd := rand.New(rand.NewSource(42))
-	sim := agent.NewSim(ctx, t, agent.SimConfig{
+	sim := agent.NewSim(ctx, t, newBlockStore, agent.SimConfig{
 		Seed:             rnd.Int63(),
 		CheckpointEpochs: 1000,
 	})
@@ -152,7 +154,7 @@ func TestCommitPowerAndCheckInvariants(t *testing.T) {
 	minerCount := 1
 
 	rnd := rand.New(rand.NewSource(42))
-	sim := agent.NewSim(ctx, t, agent.SimConfig{Seed: rnd.Int63()})
+	sim := agent.NewSim(ctx, t, newBlockStore, agent.SimConfig{Seed: rnd.Int63()})
 	accounts := vm_test.CreateAccounts(ctx, t, sim.GetVM(), minerCount, initialBalance, rnd.Int63())
 	sim.AddAgent(agent.NewMinerGenerator(
 		accounts,
@@ -207,7 +209,7 @@ func TestCommitAndCheckReadWriteStats(t *testing.T) {
 
 	// set up sim
 	rnd := rand.New(rand.NewSource(42))
-	sim := agent.NewSim(ctx, t, agent.SimConfig{
+	sim := agent.NewSim(ctx, t, newBlockStore, agent.SimConfig{
 		Seed:             rnd.Int63(),
 		CheckpointEpochs: 1000,
 	})
@@ -284,7 +286,7 @@ func TestCreateDeals(t *testing.T) {
 
 	// set up sim
 	rnd := rand.New(rand.NewSource(42))
-	sim := agent.NewSim(ctx, t, agent.SimConfig{Seed: rnd.Int63()})
+	sim := agent.NewSim(ctx, t, newBlockStore, agent.SimConfig{Seed: rnd.Int63()})
 
 	// create miners
 	workerAccounts := vm_test.CreateAccounts(ctx, t, sim.GetVM(), minerCount, initialBalance, rnd.Int63())
@@ -357,7 +359,7 @@ func TestCCUpgrades(t *testing.T) {
 
 	// set up sim
 	rnd := rand.New(rand.NewSource(42))
-	sim := agent.NewSim(ctx, t, agent.SimConfig{Seed: rnd.Int63()})
+	sim := agent.NewSim(ctx, t, newBlockStore, agent.SimConfig{Seed: rnd.Int63()})
 
 	// create miners
 	workerAccounts := vm_test.CreateAccounts(ctx, t, sim.GetVM(), minerCount, initialBalance, rnd.Int63())
@@ -428,6 +430,10 @@ func TestCCUpgrades(t *testing.T) {
 				sim.MessageCount, deals, upgrades)
 		}
 	}
+}
+
+func newBlockStore() cbor.IpldBlockstore {
+	return ipld.NewBlockStoreInMemory()
 }
 
 func printCallStats(method vm_test.MethodKey, stats *vm_test.CallStats, indent string) { // nolint:unused