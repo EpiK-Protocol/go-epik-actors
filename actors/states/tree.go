filecoin-project/specs-actors

Make Map bitwidth configurable for each instance (#1320)

actors/states/tree.go
@@ -7,6 +7,7 @@ import (
 	"github.com/ipfs/go-cid"
 	"golang.org/x/xerrors"
 
+	"github.com/filecoin-project/specs-actors/v3/actors/builtin"
 	"github.com/filecoin-project/specs-actors/v3/actors/util/adt"
 )
 
@@ -28,7 +29,7 @@ type Tree struct {
 
 // Initializes a new, empty state tree backed by a store.
 func NewTree(store adt.Store) (*Tree, error) {
-	emptyMap := adt.MakeEmptyMap(store)
+	emptyMap := adt.MakeEmptyMap(store, builtin.DefaultHamtBitwidth)
 	return &Tree{
 		Map:   emptyMap,
 		Store: store,
@@ -37,7 +38,7 @@ func NewTree(store adt.Store) (*Tree, error) {
 
 // Loads a tree from a root CID and store.
 func LoadTree(s adt.Store, r cid.Cid) (*Tree, error) {
-	m, err := adt.AsMap(s, r)
+	m, err := adt.AsMap(s, r, builtin.DefaultHamtBitwidth)
 	if err != nil {
 		return nil, err
 	}

actors/util/adt/balancetable.go
@@ -6,6 +6,8 @@ import (
 	"github.com/filecoin-project/go-state-types/big"
 	cid "github.com/ipfs/go-cid"
 	"golang.org/x/xerrors"
+
+	"github.com/filecoin-project/specs-actors/v3/actors/builtin"
 )
 
 // A specialization of a map of addresses to (positive) token amounts.
@@ -14,7 +16,7 @@ type BalanceTable Map
 
 // Interprets a store as balance table with root `r`.
 func AsBalanceTable(s Store, r cid.Cid) (*BalanceTable, error) {
-	m, err := AsMap(s, r)
+	m, err := AsMap(s, r, builtin.DefaultHamtBitwidth)
 	if err != nil {
 		return nil, err
 	}

actors/util/adt/balancetable_test.go
@@ -10,6 +10,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
+	"github.com/filecoin-project/specs-actors/v3/actors/builtin"
 	"github.com/filecoin-project/specs-actors/v3/actors/util/adt"
 	"github.com/filecoin-project/specs-actors/v3/support/mock"
 	tutil "github.com/filecoin-project/specs-actors/v3/support/testing"
@@ -19,7 +20,7 @@ func TestBalanceTable(t *testing.T) {
 	buildBalanceTable := func() *adt.BalanceTable {
 		rt := mock.NewBuilder(context.Background(), address.Undef).Build(t)
 		store := adt.AsStore(rt)
-		emptyMap := adt.MakeEmptyMap(store)
+		emptyMap := adt.MakeEmptyMap(store, builtin.DefaultHamtBitwidth)
 
 		bt, err := adt.AsBalanceTable(store, tutil.MustRoot(t, emptyMap))
 		require.NoError(t, err)
@@ -128,7 +129,7 @@ func TestSubtractWithMinimum(t *testing.T) {
 	buildBalanceTable := func() *adt.BalanceTable {
 		rt := mock.NewBuilder(context.Background(), address.Undef).Build(t)
 		store := adt.AsStore(rt)
-		emptyMap := adt.MakeEmptyMap(store)
+		emptyMap := adt.MakeEmptyMap(store, builtin.DefaultHamtBitwidth)
 
 		bt, err := adt.AsBalanceTable(store, tutil.MustRoot(t, emptyMap))
 		require.NoError(t, err)

EpiK-Protocol/go-epik-actors

actors/states/tree.go
@@ -8,6 +8,7 @@ import (
 	"github.com/filecoin-project/go-address"
 	"github.com/ipfs/go-cid"
 
+	"github.com/filecoin-project/specs-actors/v2/actors/builtin"
 	"github.com/filecoin-project/specs-actors/v2/actors/util/adt"
 )
 
@@ -30,7 +31,7 @@ type Tree struct {
 
 // Initializes a new, empty state tree backed by a store.
 func NewTree(store adt.Store) (*Tree, error) {
-	emptyMap := adt.MakeEmptyMap(store)
+	emptyMap := adt.MakeEmptyMap(store, builtin.DefaultHamtBitwidth)
 	return &Tree{
 		Map:   emptyMap,
 		Store: store,
@@ -39,7 +40,7 @@ func NewTree(store adt.Store) (*Tree, error) {
 
 // Loads a tree from a root CID and store.
 func LoadTree(s adt.Store, r cid.Cid) (*Tree, error) {
-	m, err := adt.AsMap(s, r)
+	m, err := adt.AsMap(s, r, builtin.DefaultHamtBitwidth)
 	if err != nil {
 		return nil, err
 	}

actors/util/adt/balancetable.go
@@ -6,6 +6,8 @@ import (
 	"github.com/filecoin-project/go-state-types/big"
 	cid "github.com/ipfs/go-cid"
 	"golang.org/x/xerrors"
+
+	"github.com/filecoin-project/specs-actors/v2/actors/builtin"
 )
 
 // A specialization of a map of addresses to (positive) token amounts.
@@ -14,7 +16,7 @@ type BalanceTable Map
 
 // Interprets a store as balance table with root `r`.
 func AsBalanceTable(s Store, r cid.Cid) (*BalanceTable, error) {
-	m, err := AsMap(s, r)
+	m, err := AsMap(s, r, builtin.DefaultHamtBitwidth)
 	if err != nil {
 		return nil, err
 	}

actors/util/adt/balancetable_test.go
@@ -10,6 +10,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
+	"github.com/filecoin-project/specs-actors/v2/actors/builtin"
 	"github.com/filecoin-project/specs-actors/v2/actors/util/adt"
 	"github.com/filecoin-project/specs-actors/v2/support/mock"
 	tutil "github.com/filecoin-project/specs-actors/v2/support/testing"
@@ -19,7 +20,7 @@ func TestBalanceTable(t *testing.T) {
 	buildBalanceTable := func() *adt.BalanceTable {
 		rt := mock.NewBuilder(context.Background(), address.Undef).Build(t)
 		store := adt.AsStore(rt)
-		emptyMap := adt.MakeEmptyMap(store)
+		emptyMap := adt.MakeEmptyMap(store, builtin.DefaultHamtBitwidth)
 
 		bt, err := adt.AsBalanceTable(store, tutil.MustRoot(t, emptyMap))
 		require.NoError(t, err)
@@ -128,7 +129,7 @@ func TestSubtractWithMinimum(t *testing.T) {
 	buildBalanceTable := func() *adt.BalanceTable {
 		rt := mock.NewBuilder(context.Background(), address.Undef).Build(t)
 		store := adt.AsStore(rt)
-		emptyMap := adt.MakeEmptyMap(store)
+		emptyMap := adt.MakeEmptyMap(store, builtin.DefaultHamtBitwidth)
 
 		bt, err := adt.AsBalanceTable(store, tutil.MustRoot(t, emptyMap))
 		require.NoError(t, err)